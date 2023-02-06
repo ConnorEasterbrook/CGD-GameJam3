21e2017b955e631aea9aab1a641f5cd06136b48b
Tom Pasley <thomas2.pasley@live.uwe.ac.uk>
2023-02-06 21:20:50 +0000

    Core loop

---
+++ Assets/Scripts/VegetableNinja/Vegetable.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Vegetable : MonoBehaviour
+{
+    public GameObject whole;
+    public GameObject sliced;
+
+    private Rigidbody VegetableRigidbody;
+    private Collider VegetableCollider;
+    [SerializeField] private ParticleSystem juiceEffect;
+    public int points = 1;
+
+    private void Awake()
+    {
+        VegetableRigidbody = GetComponent<Rigidbody>();
+        VegetableCollider = GetComponent<Collider>();
+        juiceEffect = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Slice(Vector3 direction, Vector3 position, float force)
+    {
+       FindObjectOfType<GameManager>().IncreaseScore(points);
+
+        // Disable the whole Vegetable
+        VegetableCollider.enabled = false;
+        whole.SetActive(false);
+
+        // Enable the sliced Vegetable
+        sliced.SetActive(true);
+
+        // Rotate based on the slice angle
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        sliced.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        Rigidbody[] slices = sliced.GetComponentsInChildren<Rigidbody>();
+
+
+        // Add a force to each slice based on the blade direction
+        foreach (Rigidbody slice in slices)
+        {
+            slice.velocity = VegetableRigidbody.velocity;
+            slice.AddForceAtPosition(direction * force, position, ForceMode.Impulse);
+            juiceEffect.Play();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Blade blade = other.GetComponent<Blade>();
+            Slice(blade.direction, blade.transform.position, blade.sliceForce);
+        }
+    }
+
+}
